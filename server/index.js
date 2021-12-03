1fe65af128e9565e619c181c77f261089faa94e9
a.bylkov <a.bylkov@huskyjam.com>
2021-12-03 16:59:03 +0700

    add auth backend

--- server/index.js
+++ server/index.js
@@ -1,26 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose');
+const router = require('./routers/index')
+const errorMiddleware = require('./middlewares/error-middleware');
 
-const authRouter = require('./routers/authRouter')
-
-const PORT = process.env.PORT || 8080
-const PASSWORD = `97bw9YqFABUPpB9`
-
+const PORT = process.env.PORT || 5000;
 const app = express()
 
-app.use(cors())
-app.use(express.json())
-app.use('/auth', authRouter)
-
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
+app.use('/api', router);
+app.use(errorMiddleware);
 
 const start = async () => {
-  try {
-    await mongoose.connect(`mongodb+srv://admin:${PASSWORD}@kanban-desk.1cwt0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
-    app.listen(PORT, () => console.log(`server started on port ${PORT}`))
-  } catch(e) {
-    console.log(e);
-  }
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 start()
